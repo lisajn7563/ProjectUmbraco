3825493d776ee1706bc4e1ed5207370d24e6defe
Lisa Johansson <lisa.jn@hotmail.com>
2024-09-23 11:27:09 +0200

    Add project files.

---
+++ Controllers/StartController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Nackademin_Umbraco.Models.ViewModels;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace Nackademin_Umbraco.Controllers
+{
+    public class StartController : RenderController
+    {
+        private readonly IUmbracoContextAccessor _umbracoContextAccessor;
+        public StartController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+            _umbracoContextAccessor = umbracoContextAccessor;
+        }
+
+        public override IActionResult Index()
+        {
+            var startPage = CurrentPage as Start;
+
+            if (startPage != null)
+            {
+                var model = new StartPageViewModel(startPage, _umbracoContextAccessor);
+
+                return View("start", model);
+            }
+            return null;
+        }
+    }
+}
---
+++ Models/ViewModels/StartPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace Nackademin_Umbraco.Models.ViewModels
+{
+    public class StartPageViewModel : BasePageModel<Start>
+    {
+        public StartPageViewModel(Start content, IUmbracoContextAccessor umbracoContextAccessor) : base(content, umbracoContextAccessor)
+        {
+        }
+    }
+}
