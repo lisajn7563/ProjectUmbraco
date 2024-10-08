77e5fbec4ccc697d8bd61fe95cc75a9f61d91c2a
Lisa Johansson <lisa.jn@hotmail.com>
2024-10-08 10:09:59 +0200

    error page

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Nackademin_Umbraco.Models.ViewModels;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace Nackademin_Umbraco.Controllers
+{
+    public class ErrorController : RenderController
+    {
+        private readonly IUmbracoContextAccessor _umbracoContextAccessor;
+        public ErrorController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+            _umbracoContextAccessor = umbracoContextAccessor;
+        }
+
+        public override IActionResult Index()
+        {
+            var errorPage = CurrentPage as Error;
+
+            if (errorPage != null)
+            {
+                var model = new ErrorPageViewModel(errorPage, _umbracoContextAccessor);
+
+                return View("error", model);
+            }
+            return null;
+        }
+    }
+}
---
+++ Models/ViewModels/ErrorPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace Nackademin_Umbraco.Models.ViewModels
+{
+    public class ErrorPageViewModel : BasePageModel<Error>
+    {
+        public ErrorPageViewModel(Error content, IUmbracoContextAccessor umbracoContextAccessor) : base(content, umbracoContextAccessor)
+        {
+        }
+    }
+}
